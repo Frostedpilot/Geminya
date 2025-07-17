313bcd6c2d8e251acfab22c43efd394807d57a5f
Frostedpilot <Truong.VT225535@sis.hust.edu.vn>
2025-07-17 21:17:08 +0700

    Better line splitting

--- utils/utils.py
+++ utils/utils.py
@@ -14,24 +14,40 @@ def get_sys_prompt():
 def split_response(response: str, max_len=1999) -> list[str]:
     chunks = response.split("\n")
     shards = []
+    current_chunk = ""
 
     for chunk in chunks:
-        if len(chunk) < max_len:
-            shards.append(chunk)
-            continue
+        tmp = current_chunk + chunk + "\n"
 
-        # Search backwards from the character limit until we found a sentence ending marker
-        while len(chunk) > max_len:
-            current_shard = chunk[:max_len]
-            for i in range(max_len - 1, -1, -1):
-                if chunk[i] in SENTENCE_ENDINGS:
-                    current_shard = chunk[: i + 1]
-                    break
-            if current_shard:
-                shards.append(current_shard)
-                chunk = chunk[len(current_shard) :]
+        # First, check the combined chunk length
+        if len(tmp) > max_len:
+            # If it exceeds max_len, split at the last sentence ending and add to shards, this requires current_chunk to be less than max_len
+            if current_chunk:
+                shards.append(current_chunk.strip())
 
-        if chunk:
-            shards.append(chunk)
+            # After added, assign the chunk to current_chunk (may be > max_len) *a
+            current_chunk = chunk + "\n"
+        else:
+            # If it fits, assign it to current_chunk (always less than max_len)
+            current_chunk = tmp
+
+        # Need to solve the problem of current_chunk being longer than max_len
+        if len(current_chunk) > max_len:
+            # Should only happen with flow after *a, meaning chunk is longer than max_len
+
+            # Split current_chunk into smaller chunks by searching backwards for sentence endings from the max_len point
+            while len(current_chunk) > max_len:
+                current_shard = current_chunk[:max_len]
+                for i in range(max_len - 1, -1, -1):
+                    if current_chunk[i] in SENTENCE_ENDINGS:
+                        current_shard = current_chunk[: i + 1]
+                        break
+                if current_shard:
+                    shards.append(current_shard)
+                    current_chunk = current_chunk[len(current_shard) :]
+
+    # Add any remaining current_chunk to shards
+    if current_chunk:
+        shards.append(current_chunk.strip())
 
     return shards
